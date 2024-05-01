b563bc2d434449f60c5a3ecb7bc0b119248446af
Zihan Jian <u7174903@anu.edu.au>
2024-05-02 02:42:24 +1000

    config

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ DATABASE_ID = os.getenv("DATABASE_ID")
 
 # Mailbox config
 SMTP_SERVER = os.getenv("SMTP_SERVER")
-smtp_port_str = 587
+SMTP_PORT = 587
 EMAIL_SENDER = os.getenv("EMAIL_SENDER")
 EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
 EMAIL_RECEIVER = os.getenv("EMAIL_RECEIVER")
