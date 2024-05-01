820d1dd1d7867cff88594cdda22557d72afdb74d
Zihan Jian <u7174903@anu.edu.au>
2024-05-01 20:26:18 +1000

    config

--- config_template.py
+++ config_template.py
@@ -1,21 +1,26 @@
-import os
-
 # Notion config
-NOTION_TOKEN = os.getenv('NOTION_TOKEN')
-DATABASE_ID = os.getenv('DATABASE_ID')
+NOTION_TOKEN = "your_notion_api_key"
+DATABASE_ID = "your_task_datebase_id"
 
 # Mailbox config
-SMTP_SERVER = os.getenv('SMTP_SERVER')
-SMTP_PORT = int(os.getenv('SMTP_PORT', 587))  # Default to 587 if not specified
-EMAIL_SENDER = os.getenv('EMAIL_SENDER')
-EMAIL_PASSWORD = os.getenv('EMAIL_PASSWORD')
-EMAIL_RECEIVER = os.getenv('EMAIL_RECEIVER')
+SMTP_SERVER = ""
+SMTP_PORT = 587  # Default to 587 if not specified
+EMAIL_SENDER = "sender_email@example.com"
+EMAIL_PASSWORD = "sender_pasword"
+EMAIL_RECEIVER = "receiver_email@example.com"
 
 # OpenAI GPT api
-OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+OPENAI_API_KEY = "your_openai_api_key"
 GPT_VERSION = "gpt-3.5-turbo"
-USERNAME = os.getenv('USERNAME')  # The name you want GPT to call you
 
-# hyper parameter
-DEFINE_DATE = os.getenv('DEFINE_DATE') #选择需要计划的日期，格式为 "YYYY-MM-DD"，如果为空就是今天
-SCHEDUAL_PROMPT = os.getenv('SCHEDUAL_PROMPT') # 如果不被打断的日常习惯安排
+# wheather api
+OPENWEATHER_API_KEY = "your_openweather_api_key"
+
+# 个persn
+SCHEDUAL_PROMPT = "Wake up at 8:00, go to bed at 22:00, two hours of fitness from 19:00-21:00" # If the daily habitual arrangement is not interrupted
+USERNAME = "Zylan" # what GPT calls you
+PRESENT_LOCATION = "Canberra" # Geolocation settings for weather notifications
+
+# 超参数
+DEFINE_DATE = "" # Select the date you need to schedule, the format is "YYYY-MM-DD", if it is empty it is today
+EMAIL_TITTLE = "Zylan's Schedule for" # The title of the email, which will be immediately followed by the date
