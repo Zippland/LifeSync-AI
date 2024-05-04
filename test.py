78a132241495b608d127d6741f1827f46951e7c3
Zihan <u7174903@anu.edu.au>
2024-05-04 23:51:06 +1000

    yml

--- test.py
+++ test.py
@@ -1,52 +1,52 @@
 import pytz
-from src.email.format_email import format_email
-from get_task.task_from_notion import fetch_tasks_from_notion
-from src.email.email_notifier import send_email
+from src.send_email.format_email import format_email
+from src.get_task.task_from_notion import fetch_tasks_from_notion
+from src.send_email.email_notifier import send_email
 from src.gpt_advice_generator import generate_advice_with_gpt
 from src.get_wheather import get_weather
 from datetime import datetime
-from config import DEFINE_DATE, TIME_ZONE
+from src.get_env.env_from_notion import get_user_env_vars
 
-# Determine the custom date; default to today if DEFINE_DATE is not set or invalid
-if DEFINE_DATE:
-    try:
-        custom_date = datetime.strptime(DEFINE_DATE, '%Y-%m-%d').date()
-    except ValueError:
-        print("Invalid date format in DEFINE_DATE. It should be 'YYYY-MM-DD'. Using today's date instead.")
-        custom_date = datetime.now().date()
-else:
-    # Get the current time in UTC, and then convert to the specified UTC offset
-    utc_now = datetime.now(pytz.utc)
-    tz = pytz.timezone('Etc/GMT-' + TIME_ZONE)
-    custom_date = utc_now.astimezone(tz).date()
+# Get the current time in UTC, and then convert to the specified UTC offset
+utc_now = datetime.now(pytz.utc)
+user_data = get_user_env_vars()
 
-today_tasks = fetch_tasks_from_notion(custom_date, "today")
-future_tasks = fetch_tasks_from_notion(custom_date, "future")
-weather = get_weather()
+for user_id in user_data:
+    user_notion_token = user_data[user_id]["USER_NOTION_TOKEN"]
+    user_database_id = user_data[user_id]["USER_DATABASE_ID"]
+    gpt_version = user_data[user_id]["GPT_VERSION"]
+    present_location = user_data[user_id]["PRESENT_LOCATION"]
+    user_career = user_data[user_id]["USER_CAREER"]
+    schedule_prompt = user_data[user_id]["SCHEDULE_PROMPT"]
+    custom_date = utc_now.astimezone(pytz.timezone('Etc/GMT-' + user_data[user_id]["TIME_ZONE"])).date()
 
-no_format_advice = ""
-formated_advice = format_email()
+    today_tasks = fetch_tasks_from_notion(custom_date, user_notion_token, user_database_id, "today")
+    future_tasks = fetch_tasks_from_notion(custom_date, user_notion_token, user_database_id, "future")
+    weather = get_weather(present_location)
 
-# weather
-adviece_weather = generate_advice_with_gpt(weather,"1")
-formated_advice += format_email(adviece_weather)
-no_format_advice += adviece_weather
-# ontline of task
-advice_outline = generate_advice_with_gpt(today_tasks,"2")
-formated_advice += format_email(advice_outline)
-no_format_advice += advice_outline
-# task time stamp
-advice_timestamp = generate_advice_with_gpt(today_tasks,"3")
-formated_advice += format_email(advice_timestamp)
-no_format_advice += advice_timestamp
-# future task
-advice_future = generate_advice_with_gpt(future_tasks,"4")
-formated_advice += format_email(advice_future)
-no_format_advice += advice_future
-# other advice
-advice_others = generate_advice_with_gpt(no_format_advice,"5")
-formated_advice += format_email(advice_others,True)
+    no_format_advice = ""
+    formated_advice = format_email()
+
+    # weather
+    adviece_weather = generate_advice_with_gpt("1", weather, gpt_version, present_location, user_career)
+    formated_advice += format_email(adviece_weather)
+    no_format_advice += adviece_weather
+    # ontline of task
+    advice_outline = generate_advice_with_gpt("2", today_tasks, gpt_version, present_location, user_career)
+    formated_advice += format_email(advice_outline)
+    no_format_advice += advice_outline
+    # task time stamp
+    advice_timestamp = generate_advice_with_gpt("3", today_tasks, gpt_version, present_location, user_career, schedule_prompt)
+    formated_advice += format_email(advice_timestamp)
+    no_format_advice += advice_timestamp
+    # future task
+    advice_future = generate_advice_with_gpt("4", future_tasks, gpt_version, present_location, user_career)
+    formated_advice += format_email(advice_future)
+    no_format_advice += advice_future
+    # other advice
+    advice_others = generate_advice_with_gpt("5", no_format_advice, gpt_version, present_location, user_career)
+    formated_advice += format_email(advice_others, USER_NAME = user_data[user_id]["USER_NAME"], ending = True)
 
 
-email_body = f"{formated_advice}"
-send_email(email_body)
+    email_body = f"{formated_advice}"
+    send_email(email_body, user_data[user_id]["EMAIL_RECEIVER"], user_data[user_id]["EMAIL_TITLE"])
