9d56d315e35a394054ffeae08e397e21b2be3bc8
Zihan Jian <u7174903@anu.edu.au>
2024-05-07 19:20:25 +1000

    debug

--- src/get_env/env_from_notion.py
+++ src/get_env/env_from_notion.py
@@ -1,28 +1,33 @@
 from notion_client import Client
 from config import ENV_NOTION_TOKEN, ENV_DATABASE_ID
 
-def get_user_env_vars():
-    print("get users' enviroments' variables")
-    # 查询数据库
+def get_user_env_var(user_id):
+    print("Fetching user's environment variables")
+    # Initialize Notion client
     notion = Client(auth=ENV_NOTION_TOKEN)
-    response = notion.databases.query(ENV_DATABASE_ID)
+    
+    # Query for a specific user's data using their USER_ID
+    response = notion.databases.query(
+        **{
+            "database_id": ENV_DATABASE_ID,
+            "filter": {
+                "property": "USER_ID",
+                "title": {
+                    "equals": user_id
+                }
+            }
+        }
+    )
 
-    # 存储用户环境变量的字典
-    user_env_vars = {}
-
-    # 遍历每个页面（数据库中的行）
-    for page in response.get("results", []):
-        user_id = page['properties']['USER_ID']['title'][0]['text']['content']
-        # 假设数据库中有如下属性
-        user_env_vars[user_id] = {
-            # Aunthentication
-            "USER_PASSWORD": page['properties']['USER_PASSWORD']['rich_text'][0]['text']['content'],
-            # personal info
+    # Check if any pages are found
+    pages = response.get("results", [])
+    if pages:
+        page = pages[0]  # Assume the first result is the desired one
+        user_env_vars = {
             "USER_NAME": page['properties']['USER_NAME']['rich_text'][0]['text']['content'],
             "USER_CAREER": page['properties']['USER_CAREER']['rich_text'][0]['text']['content'],
             "PRESENT_LOCATION": page['properties']['PRESENT_LOCATION']['rich_text'][0]['text']['content'],
             "SCHEDULE_PROMPT": page['properties']['SCHEDULE_PROMPT']['rich_text'][0]['text']['content'],
-            # hyper parameter
             "GPT_VERSION": page['properties']['GPT_VERSION']['rich_text'][0]['text']['content'],
             "USER_NOTION_TOKEN": page['properties']['USER_NOTION_TOKEN']['rich_text'][0]['text']['content'],
             "USER_DATABASE_ID": page['properties']['USER_DATABASE_ID']['rich_text'][0]['text']['content'],
@@ -30,24 +35,9 @@ def get_user_env_vars():
             "TIME_ZONE": page['properties']['TIME_ZONE']['rich_text'][0]['text']['content'],
             "EMAIL_TITLE": page['properties']['EMAIL_TITLE']['rich_text'][0]['text']['content']
         }
-    print("Enviroments' variables fetched.")
-    print(user_env_vars)
-    return user_env_vars
-
-# # 假设已经运行了 get_user_env_vars 函数并保存了返回值
-# user_data = get_user_env_vars()
-
-# # 指定用户ID
-# user_id = "user123"
-
-# # 检查是否有这个用户的数据
-# if user_id in user_data:
-#     # 获取特定用户的用户名
-#     username = user_data[user_id]["USERNAME"]
-#     # 获取特定用户的时区
-#     time_zone = user_data[user_id]["TIME_ZONE"]
-    
-#     print(f"Username for {user_id}: {username}")
-#     print(f"Time Zone for {user_id}: {time_zone}")
-# else:
-#     print(f"No data available for user ID: {user_id}")
+        print("Environment variables fetched.")
+        print(user_env_vars)
+        return user_env_vars
+    else:
+        print("No data available for user ID:", user_id)
+        return None
