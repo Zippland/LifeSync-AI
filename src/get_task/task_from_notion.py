da0ea19d6b10a79e033c6fcc0039a6bc24e9a2b6
Zihan <u7174903@anu.edu.au>
2024-05-10 04:31:01 +1000

    m

--- src/get_task/task_from_notion.py
+++ src/get_task/task_from_notion.py
@@ -8,7 +8,7 @@ def fetch_tasks_from_notion(custom_date, USER_NOTION_TOKEN, USER_DATABASE_ID, mo
     try:
         results = notion.databases.query(
             database_id=USER_DATABASE_ID,
-            filter={"property": " Complete",
+            filter={"property": "Complete",
                     "checkbox": {
                         "equals": False
                     }
@@ -19,7 +19,6 @@ def fetch_tasks_from_notion(custom_date, USER_NOTION_TOKEN, USER_DATABASE_ID, mo
         # Define the date range based on the mode
         if mode == "today":
             date_end = custom_date
-            custom_date -= timedelta(days=7) 
         elif mode == "future":
             date_end = custom_date + timedelta(days=30)
             custom_date += timedelta(days=1)
@@ -31,14 +30,22 @@ def fetch_tasks_from_notion(custom_date, USER_NOTION_TOKEN, USER_DATABASE_ID, mo
             if 'date' in row['properties']['Date'] and row['properties']['Date']['date']:
                 task_date = datetime.strptime(row['properties']['Date']['date']['start'], '%Y-%m-%d').date()
                 # Check if the task date falls within the defined range
-                if custom_date <= task_date <= date_end:
-                    task = {
-                        'Name': row['properties']['Name']['title'][0]['text']['content'] if row['properties']['Name']['title'] else 'No name',
-                        'Description': row['properties']['Description']['rich_text'][0]['text']['content'] if 'rich_text' in row['properties']['Description'] and row['properties']['Description']['rich_text'] else 'No description',
-                        'Date': task_date.strftime('%Y-%m-%d')  # Date added to each task
-                    }
-                    tasks.append(task)
-        # print(tasks)
+                if mode == "future":
+                    if custom_date <= task_date <= date_end:
+                        task = {
+                            'Name': row['properties']['Name']['title'][0]['text']['content'] if row['properties']['Name']['title'] else 'NA',
+                            'Description': row['properties']['Description']['rich_text'][0]['text']['content'] if 'rich_text' in row['properties']['Description'] and row['properties']['Description']['rich_text'] else 'NA',
+                            'Date': task_date.strftime('%Y-%m-%d')  # Date added to each task
+                        }
+                        tasks.append(task)
+                else:
+                    if task_date <= date_end:
+                        task = {
+                            'Name': row['properties']['Name']['title'][0]['text']['content'] if row['properties']['Name']['title'] else 'NA',
+                            'Description': row['properties']['Description']['rich_text'][0]['text']['content'] if 'rich_text' in row['properties']['Description'] and row['properties']['Description']['rich_text'] else 'NA',
+                        }
+                        tasks.append(task)
+        print(tasks)
         print("Fetching success.")
         return tasks
     except Exception as e:
