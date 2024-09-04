cd327f6853fb43fd7da7595e5f18d99fe39c81d2
Zylan <zihan.jian@outlook.com>
2024-09-04 08:12:54 +0800

    tittle

--- morning_email.py
+++ morning_email.py
@@ -47,6 +47,7 @@ for user_id in user_data:
     advice = email_advice_with_ai(data, gpt_version, present_location, user_career, local_time, schedule_prompt)
     print("Fimal advice:\n" + advice)
 
-    email_body = f"{format_email(advice, user_name,"日程晨报")}"
+    tittle = "日程晨报"
+    email_body = f"{format_email(advice, user_name, tittle)}"
     send_email(email_body, user_data[user_id]["EMAIL_RECEIVER"], user_data[user_id]["EMAIL_TITLE"])
 
